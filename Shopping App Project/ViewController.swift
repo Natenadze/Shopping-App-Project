83abe9c23337b840b1d9bbbb7e28b328ea4e028c
davit <davit.natenadze@gmail.com>
2023-01-15 18:59:42 +0100

    Initial Commit

---
+++ Shopping App Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shopping App Project
+//
+//  Created by Davit Natenadze on 15.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
