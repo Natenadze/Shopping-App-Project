6ba8a2f8d66a71a6b4391cff6c8341f8bd0b80de
davit <davit.natenadze@gmail.com>
2023-01-17 18:01:09 +0100

    can display the fetched data

--- Shopping App Project/View/ShoppingPage.swift
+++ Shopping App Project/View/ShoppingPage.swift
@@ -8,32 +8,48 @@
 import UIKit
 
 class ShoppingPage: UIViewController {
-
+    
+    var items: ProductModel!
+    
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        NetworkManager.performURLRequest("https://dummyjson.com/products") { (data: ProductModel)  in
-//            print(data.products[0].title)
-//        }
-        tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "cell")
-        navigationController?.navigationBar.isHidden = true
-        tableView.delegate = self
-        tableView.dataSource = self 
+      print("viewDidLoad")
+    
+        NetworkManager.performURLRequest("https://dummyjson.com/products") { (data: ProductModel)  in
+            self.items = data
+         
+ 
+        }
     }
     
-   
+    override func viewDidAppear(_ animated: Bool) {
+        print("Did Appear")
+        tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
+        navigationController?.navigationBar.isHidden = true
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
     
-
+    
+    
+    
 }
 
 extension ShoppingPage: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        5
+     
+        return items.products.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomCell
+        
+        cell.titleLabel.text = items.products[indexPath.row].title
+        cell.stockLabel.text = String(items.products[indexPath.row].stock)
+        cell.priceLabel.text = String(items.products[indexPath.row].price)
         
         return cell
     }
@@ -43,18 +59,20 @@ extension ShoppingPage: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 1))
-        header.backgroundColor = .gray
-        tableView.tableHeaderView = header
+        let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
+        header.backgroundColor = .white
+        let image = UIImageView(image: UIImage(named: "logo"))
+        image.contentMode = .scaleAspectFit
+        header.addSubview(image)
+        image.frame = CGRect(x: 0, y: 5, width: header.frame.size.width, height: 100)
+
         return header
     }
-    
-    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 120))
-        footerView.backgroundColor = .red
-        tableView.tableFooterView = footerView
-        return footerView
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        100
     }
+    
     
     
     
