a385afec376543cbf58645611f6edaf7ed992ec2
davit <davit.natenadze@gmail.com>
2023-01-18 16:14:37 +0100

    headers work partially

--- Shopping App Project/View/ShoppingPage.swift
+++ Shopping App Project/View/ShoppingPage.swift
@@ -9,50 +9,64 @@ import UIKit
 
 class ShoppingPage: UIViewController {
     
-    var items: ProductModel!
-
+    var items: [Product]!
+    var groupedItems = [[Product]]()
+    
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         NetworkManager.performURLRequest("https://dummyjson.com/products") { (data: ProductModel)  in
-            self.items = data
+            self.items = data.products
+            print(data.products)
+            let dict = Dictionary(grouping: self.items, by: {$0.category})
+            let keys = dict.keys
+            DispatchQueue.main.async {
+                keys.forEach { self.groupedItems.append(dict[$0]!) }
+            }
+            
+          
+            
         }
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        print("Did Appear")
+        
         tableView.register(UINib(nibName: "CustomCell", bundle: nil), forCellReuseIdentifier: "cell")
         
         navigationController?.navigationBar.isHidden = true
         tableView.delegate = self
         tableView.dataSource = self
-
+        
     }
     
     
 }
 
 extension ShoppingPage: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        groupedItems.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-     
-        return items.products.count
+        
+        return groupedItems[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomCell
         
-        cell.titleLabel.text = items.products[indexPath.row].title
-        cell.stockLabel.text = String(items.products[indexPath.row].stock)
-        cell.priceLabel.text = String(items.products[indexPath.row].price)
+        cell.titleLabel.text = items[indexPath.row].title
+        cell.stockLabel.text = String(items[indexPath.row].stock)
+        cell.priceLabel.text = String(items[indexPath.row].price)
         
-        URLSession.shared.dataTask(with: URL(string: items.products[indexPath.row].thumbnail)!) { data, response, error in
-                if let data = data {
-                    DispatchQueue.main.async {
-                        cell.imageView?.image = UIImage(data: data)
-                    }
+        URLSession.shared.dataTask(with: URL(string: items[indexPath.row].thumbnail)!) { data, response, error in
+            if let data = data {
+                DispatchQueue.main.async {
+                    cell.imageView?.image = UIImage(data: data)
                 }
+            }
         }.resume()
         
         return cell
@@ -65,15 +79,34 @@ extension ShoppingPage: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 100))
         header.backgroundColor = .white
-        let image = UIImageView(image: UIImage(named: "logo"))
-        image.contentMode = .scaleAspectFit
-        header.addSubview(image)
-        image.frame = CGRect(x: 0, y: 5, width: header.frame.size.width, height: 100)
-
+        if section == 0 {
+            
+            let image = UIImageView(image: UIImage(named: "logo"))
+            image.contentMode = .scaleAspectFit
+            header.addSubview(image)
+            image.frame = CGRect(x: 0, y: 5, width: header.frame.size.width, height: 100)
+            
+        }else {
+            let label = UILabel(frame: CGRect(x: 5, y: 5,
+                                              width: header.frame.size.width - 15,
+                                              height: header.frame.size.height - 10))
+            label.text = groupedItems[section][0].category
+            label.font = .boldSystemFont(ofSize: 20)
+            header.addSubview(label)
+            
+        }
+       
+      
         return header
     }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "asdasdkasjdnasd"
+    }
+    
 
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         100
-    }  
+    }
 }
